129054e6a364ae5ed47771f0c3660a4be80cc0dc
Jamie Tobin <jamie.tobin@gmail.com>
2017-08-17 09:19:08 +0100

    Final Changes from course.
     Working version with JPA.

--- src/main/java/com/mycomp/springbootstarter/course/Course.java
+++ src/main/java/com/mycomp/springbootstarter/course/Course.java
@@ -1,27 +1,43 @@
-package com.mycomp.springbootstarter.topic;
+package com.mycomp.springbootstarter.course;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.mycomp.springbootstarter.topic.Topic;
 
 @Entity
-public class Topic {
+public class Course {
 
 	@Id
 	private String id;
 	private String name;
 	private String description;
 	
-	public Topic(){
+	@ManyToOne
+	private Topic topic;
+	
+	public Course(){
 		
 	};
 	
-	public Topic(String id, String name, String description) {
+	public Course(String id, String name, String description, String topicId) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.description = description;
+		this.topic = new Topic(topicId, "", "");
+		//change this for name and desc later
 	}
 	
+	public Topic getTopic() {
+		return topic;
+	}
+
+	public void setTopic(Topic topic) {
+		this.topic = topic;
+	}
+
 	public String getId() {
 		return id;
 	}
